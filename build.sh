194fc3daea82581a6d81fd7dab0f2e4f739790f7
wyatt <wyatt.lemaster@gmail.com>
2023-11-30 22:08:29 +0000

    first commit

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+ 
+ oc new-project wwl0004-hw3-10
+ oc create -f hw3Template.json
+ oc process php-employees-persistent | oc create -f -
+ 
+echo "sleeping for 1 minute to give pods time to get ready..."
+sleep 10
+echo " 50 remaining "
+sleep 10
+echo " 40 remaining "
+sleep 10
+echo " 30 remaining "
+sleep 10
+echo " 20 remaining "
+sleep 10
+echo " 10 remaining "
+sleep 10
+echo " done sleeping "
+sleep 1
+
+
+ oc expose svc php-employees
+
+ oc get route
+ 
+ echo " please enter the above route into this curl command: "
+ echo " curl -w "\n" http://quote-php-test4.apps.na410r.prod.ole.redhat.com "
